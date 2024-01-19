5092c8e4fbf140a276786c7a2586d9e0be91945d
Nurul Amin <namin2750@gmail.com>
2024-01-19 23:01:45 +0600

    working api

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,14 +1,14 @@
-// // const cloudinary = require("cloudinary").v2;
-// import cloudinary from "cloudinary";
-// import {
-//   PUBLIC_CLOUDINARY_API_KEY,
-//   PUBLIC_CLOUDINARY_CLOUD_NAME,
-// } from "$env/static/public";
-// import { CLOUDINARY_API_SECRET } from "$env/static/private";
+// const cloudinary = require("cloudinary").v2;
+import cloudinary from "cloudinary";
+import {
+  PUBLIC_CLOUDINARY_API_KEY,
+  PUBLIC_CLOUDINARY_CLOUD_NAME,
+} from "$env/static/public";
+import { CLOUDINARY_API_SECRET } from "$env/static/private";
 
-// cloudinary.config({
-//   cloud_name: PUBLIC_CLOUDINARY_CLOUD_NAME,
-//   api_key: PUBLIC_CLOUDINARY_API_KEY,
-//   api_secret: CLOUDINARY_API_SECRET,
-//   secure: true,
-// });
+cloudinary.config({
+  cloud_name: PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+  secure: true,
+});
--- src/routes/api/images/+server.js
+++ src/routes/api/images/+server.js
@@ -2,6 +2,10 @@ import { json } from "@sveltejs/kit";
 import cloudinary from "cloudinary";
 
 export async function GET() {
-  // const result = await cloudinary.v2.api.resource();
-  return json({ data: "images list" });
+  const { resources } = await cloudinary.v2.api.resources();
+  return json({
+    data: resources.map((public_id) => {
+      return { public_id };
+    }),
+  });
 }
