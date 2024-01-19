7b403c65c4d035155fb201cb5e5dc10a9656198c
Nurul Amin <namin2750@gmail.com>
2024-01-19 17:12:18 +0600

    basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,22 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>
+import "../app.pcss";
+import { Camera } from 'lucide-svelte';
+import { Upload } from 'lucide-svelte';
+import Button from "$lib/components/ui/button/button.svelte";
+</script>
+<div class="border-b fixed top-0 w-full bg-white">
+  <div class="container h-16 flex items-center">
+    <a href="/">
+      <Camera />
+    </a>
+    <div class="ml-auto">
+      <Button class="space-x-1"><Upload  class="h-[1rem]"/>
+        <span>Upload</span>
+      </Button>
+    </div>
+  </div>
+</div>
+
+<div class="container pt-16">
+  <slot></slot>
+</div>
