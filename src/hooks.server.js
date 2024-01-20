cf725d5eda75b5cb81a730e5a1c76f4c94e26a76
Nurul Amin <namin2750@gmail.com>
2024-01-20 21:24:19 +0600

    some fixes in config

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,14 +1,14 @@
 // const cloudinary = require("cloudinary").v2;
 import cloudinary from "cloudinary";
 import {
-  PUBLIC_CLOUDINARY_API_KEY,
-  PUBLIC_CLOUDINARY_CLOUD_NAME,
+  VITE_PUBLIC_CLOUDINARY_API_KEY,
+  VITE_PUBLIC_CLOUDINARY_CLOUD_NAME,
 } from "$env/static/public";
-import { CLOUDINARY_API_SECRET } from "$env/static/private";
+import { CLOUDINARY_SECRET } from "$env/static/private";
 
 cloudinary.config({
-  cloud_name: PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+  cloud_name: VITE_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: VITE_PUBLIC_CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_SECRET,
   secure: true,
 });
