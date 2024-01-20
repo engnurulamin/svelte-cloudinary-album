2876fcc03634d2676d3d3a9c7508deda6c879ac2
Nurul Amin <namin2750@gmail.com>
2024-01-20 22:36:06 +0600

    displayed image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
-
 <script>
+  import {CldImage} from 'svelte-cloudinary'
 export let data;
 console.log(data)
 </script>
 
-<ul>
+<div class="grid auto-cols-400 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center">
   {#each data.images as {public_id} }
-    <li>{public_id}</li>
+  <a href="/images/{public_id}" class="w-full h-full grid place-items-center">
+    <CldImage src={public_id} width="400" height="400"/>
+  </a>
   {/each}
-</ul>
+</div>
 
 
 
