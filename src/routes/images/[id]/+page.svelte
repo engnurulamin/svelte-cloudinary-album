e5f323d28f08f4f3776caecc25ef4d45c322e894
Nurul Amin <namin2750@gmail.com>
2024-01-24 23:17:21 +0600

    removed

--- src/routes/images/[id]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	// import { CldImage } from 'svelte-cloudinary';
-  
-  export let data;
-  console.log(data)
-</script>
-
-<!-- <CldImage src={data.image.public_id}/> -->
--- src/routes/images/[id]/+page.ts
+++
@@ -1,8 +0,0 @@
-export async function load({ fetch, params }) {
-  const id = params.id;
-  const response = await fetch(`/api/image?id=${id}`);
-  const result = await response.json();
-  return {
-    image: result,
-  };
-}
